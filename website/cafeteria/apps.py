8662268821434ecaa5c8144b0842c89c69e3c667
AlejandroRmz <lgsus.esc@gmail.com>
2025-11-11 20:53:18 -0600

    ahora si jala el fokin server

--- website/cafeteria/apps.py
+++ website/cafeteria/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CafeteriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Cafeteria'
+    name = 'cafeteria'
--- website/website/urls.py
+++ website/website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for piaCafeteria project.
+URL configuration for website project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
