04fddb1add28680263626a0fc0e180402e0fa2ea
luca384 <lucaramonts@gmail.com>
2025-11-12 23:28:20 -0600

    agregado de htmls, carpeta img,

--- website/cafeteria/urls.py
+++ website/cafeteria/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.index, name = 'index'),
+    path('unete/', views.unete, name = 'unete')
 ]
--- website/cafeteria/views.py
+++ website/cafeteria/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index (request):
     return render (request,'index.html')
+
+def unete(request):
+    return render(request, 'unete.html')
